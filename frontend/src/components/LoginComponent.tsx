dfd9a8e696333ce72456f4f2ac12431150c970d0
nevermore17 <nevermore.wmail@gmail.com>
2022-12-24 17:49:39 +0300

    Changed button text

--- frontend/src/components/LoginComponent.tsx
+++ frontend/src/components/LoginComponent.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Box from "@mui/material/Box";
+
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
-import Grid from "@mui/material/Grid";
 
 import { useNavigate } from "react-router-dom";
 
@@ -49,7 +48,7 @@ const LoginComponent: React.FC<ILoginComporentProps> = ({}) => {
           variant="contained"
           onClick={loginAction}
         >
-          Contained
+          Sign in
         </Button>
       </div>
     </>
