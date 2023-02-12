6100448ff8e5ac57c131dbff11442152ecbcd4fa
Ciratoku <edissonman255@gmail.com>
2023-02-12 19:08:44 +0300

    db_init

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const mongoDB = "mongodb://127.0.0.1/monitoring";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+module.exports = db;
---
+++ backend/db_init.js
@@ -0,0 +1,18 @@
+const db = require('./db');
+const Doctor = require('./models/Doctor');
+const Patient = require('./models/Patient');
+const User = require('./models/User');
+
+const newUsers = [{Name: 'Ivan Ivanov', login: 'ivan_ivanov', type: 'doctor'}, {Name: 'Petr Petrov', login: 'petr_petrov', type: 'doctor'}, {Name: 'Zidan Zidanov', login: 'z', type: 'patient'}, {Name: 'Kek W', login: 'kekl', type: 'patient'}];
+
+newUsers.forEach(async (u) => {
+    let newDoctorOrPatient = u.type === 'doctor' ? new Doctor({ name: u.Name }) : new Patient({ name: u.Name });
+    await newDoctorOrPatient.save();
+    const newUser = new User({
+        login: u.login,
+        role: u.type,
+        profile: newDoctorOrPatient._id
+    });
+    await newUser.save();
+});
+db.connection.close();
