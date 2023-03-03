117eb621d0c31200941e6b4038916a6ecc2a7d4f
Ciratoku <edissonman255@gmail.com>
2023-03-03 20:36:45 +0300

    src is array fix

--- backend/models/PatientMedia.js
+++ backend/models/PatientMedia.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PatientMediaSchema = new Schema({
-    src: {
+    src: [{
         type: String
-    },
+    }],
     create_at: {
         type: Date,
         default: Date.now
