5ef97454331ff8ee442106b0de697df2e5e5a643
Ciratoku <edissonman255@gmail.com>
2023-03-03 20:13:07 +0300

    deleted

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -46,44 +46,4 @@ router.post('/Patient/recommendation', PatientsController.CreateRecommendation);
 
 router.patch('/Patient/setDoctor', PatientsController.SetDoctor); //+
 
-router.get('/Patient/recommendation', (req, res, next) => {
-    const body = req.query.patientId;
-    console.log(body)
-    res.send([{
-        id: "new",
-        date: new Date(),
-        text: 'Рекомендация'
-    }]);
-});
-
-router.get('/Patient/unsignedMedia', (req, res, next) => {
-    const patientId = req.query.patientId;
-    res.send(['https://thumbs.dreamstime.com/b/panoramic-autumn-landscape-wooden-path-fall-nature-backgro-sunset-background-97979511.jpg']);
-});
-
-router.post('/Patient/answer', upload.any(), (req, res, next) => {
-    const data = req.body;
-   /* Тут находятся данные
-   * в данной ручки следующий объект:
-   * quizResult: QuizResult
-   * */
-
-    const files = req.files;
-    /* Тут находятся массив всех загруженных файлов
-   * Каждый элемент массива содержит следующее:
-   * {
-   * filename: string // Навряд ли понадобиться
-   * originaname: string // оригинальное полное название файла
-   * encoding: string // Навряд ли понадобиться
-   * mimetype: string // Навряд ли понадобиться
-   * destination: string // путь, куда сохранился файл(сейчас стоит ./assets/. Если надо, то выше можно поменять путь
-   * filename: string // имя этого файла в папке destination
-   * path: string // полный путь файла относительно текущей деректории
-   * size: number // размер файла
-   * }
-   * */
-    console.log(req.body, req.files);
-    res.send(['https://thumbs.dreamstime.com/b/panoramic-autumn-landscape-wooden-path-fall-nature-backgro-sunset-background-97979511.jpg']);
-})
-
 module.exports = router;
