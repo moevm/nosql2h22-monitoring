5a2de639892f0a5d3b485ab1ac98b45352469870
nevermore17 <nevermore.wmail@gmail.com>
2023-03-04 20:58:10 +0300

    feat: update Patient info view

--- frontend/src/components/PatientInfo/PatientInfo.tsx
+++ frontend/src/components/PatientInfo/PatientInfo.tsx
@@ -1,19 +1,20 @@
 import React, { FC, useState } from "react";
 import { Typography, Button, Stack } from "@mui/material";
+
 import { useAppDispatch } from "../../hooks/useRedux";
 import {
   getSignDocument,
   getUnsignedMedia,
 } from "../../redux/reducers/userReducer/userReducer";
 import DownloadMedia from "../../shared/DownloadMedia/DownloadMedia";
-import { showError } from "../../utils/showError";
-
 import DoctorQuiz from "../DoctorQuiz/DoctorQuiz";
 import DoctorRecommendations from "../DoctorRecommendations/DoctorRecommendations";
 import DoctorQuizResults from "../DoctorQuizResults/DoctorQuizResults";
 import FileLoader from "../../shared/FileLoader/FileLoader";
+import { showError } from "../../utils/showError";
 
 import { getPatient } from "./PatientInfo.hooks";
+
 import "./PatientInfo.css";
 interface IPatientInfoProps {
   patientId: string;
@@ -57,89 +58,99 @@ const PatientInfo: FC<IPatientInfoProps> = ({ patientId }) => {
             </div>
           </div>
 
-          <div className="patient-info__element">
-            <Button
-              sx={{ justifyContent: "left" }}
-              fullWidth
-              onClick={() => setQuiz(!quiz)}
-              variant={quiz ? "outlined" : "contained"}
-            >
-              <Typography variant="h3" fontSize={20}>
-                <h3>Quiz</h3>
-              </Typography>
-            </Button>
-            {quiz ? (
-              <DoctorQuiz
-                questions={patient.quiz}
-                refreshPatient={refreshPatient}
-                patientId={patientId}
-              />
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="patient-info__element">
-            <Button
-              fullWidth
-              sx={{ justifyContent: "left" }}
-              onClick={() => setRecommendations(!recommendations)}
-              variant={recommendations ? "outlined" : "contained"}
-            >
-              <Typography variant="h3" fontSize={20}>
-                <h3>Recommendations</h3>
-              </Typography>
-            </Button>
-            {recommendations ? (
-              <DoctorRecommendations
-                recommendations={patient.recommendations}
-                refreshPatient={refreshPatient}
-                patientId={patientId}
-              />
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="patient-info__element">
-            <Button
-              fullWidth
-              sx={{ justifyContent: "left" }}
-              onClick={() => setQuizResult(!quizResults)}
-              variant={quizResults ? "outlined" : "contained"}
-            >
-              <Typography variant="h3" fontSize={20}>
-                <h3>Quiz Result</h3>
-              </Typography>
-            </Button>
-            {quizResults ? (
-              <DoctorQuizResults
-                quizResults={patient.quiz_results}
-                questions={patient.quiz}
-              />
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="patient-info__element">
-            <Stack>
-              <FileLoader
-                rowFiles={rowFiles}
-                maxFiles={5}
-                setRowFiles={setRowFiles}
-                accept="image/*,video/*"
-                text="Загрузить файлы"
-              />
-              <Button variant="contained" onClick={sendAnswers}>
-                Отправить результаты
-              </Button>
+          <Stack
+            direction="row"
+            justifyContent={"space-between"}
+            padding="0 100px"
+          >
+            <Stack spacing={1}>
+              <div className="patient-info__element">
+                <Button
+                  sx={{ justifyContent: "left" }}
+                  fullWidth
+                  onClick={() => setQuiz(!quiz)}
+                  variant={quiz ? "outlined" : "contained"}
+                >
+                  <Typography variant="h3" fontSize={20}>
+                    <h3>Quiz</h3>
+                  </Typography>
+                </Button>
+                {quiz ? (
+                  <DoctorQuiz
+                    questions={patient.quiz}
+                    refreshPatient={refreshPatient}
+                    patientId={patientId}
+                  />
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="patient-info__element">
+                <Button
+                  fullWidth
+                  sx={{ justifyContent: "left" }}
+                  onClick={() => setRecommendations(!recommendations)}
+                  variant={recommendations ? "outlined" : "contained"}
+                >
+                  <Typography variant="h3" fontSize={20}>
+                    <h3>Recommendations</h3>
+                  </Typography>
+                </Button>
+                {recommendations ? (
+                  <DoctorRecommendations
+                    recommendations={patient.recommendations}
+                    refreshPatient={refreshPatient}
+                    patientId={patientId}
+                  />
+                ) : (
+                  ""
+                )}
+              </div>
             </Stack>
-          </div>
-          <div>
-            <DownloadMedia
-              fetchMedia={fetchUnsignedMedia}
-              media={unsignedMedia}
-              title={"Подписанные документы"}
-            />
-          </div>
+            <Stack>
+              <div className="patient-info__element">
+                <Button
+                  fullWidth
+                  sx={{ justifyContent: "left" }}
+                  onClick={() => setQuizResult(!quizResults)}
+                  variant={quizResults ? "outlined" : "contained"}
+                >
+                  <Typography variant="h3" fontSize={20}>
+                    <h3>Quiz Result</h3>
+                  </Typography>
+                </Button>
+                {quizResults ? (
+                  <DoctorQuizResults
+                    quizResults={patient.quiz_results}
+                    questions={patient.quiz}
+                  />
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="patient-info__element">
+                <Stack>
+                  <FileLoader
+                    rowFiles={rowFiles}
+                    maxFiles={5}
+                    setRowFiles={setRowFiles}
+                    accept="image/*,video/*"
+                    text="Загрузить файлы"
+                  />
+                  <Button variant="contained" onClick={sendAnswers}>
+                    Отправить результаты
+                  </Button>
+                </Stack>
+              </div>
+              <div>
+                <DownloadMedia
+                  fetchMedia={fetchUnsignedMedia}
+                  media={unsignedMedia}
+                  title={"Подписанные документы"}
+                />
+              </div>
+            </Stack>
+          </Stack>
         </div>
       </>
     );
