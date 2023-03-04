1ed2d3a202a87d22ab58c1986a71035a47a1e850
nevermore17 <nevermore.wmail@gmail.com>
2023-03-04 19:43:07 +0300

    fix: get BASE_URL into loade urls

--- frontend/src/shared/DownloadMedia/DownloadMedia.tsx
+++ frontend/src/shared/DownloadMedia/DownloadMedia.tsx
@@ -1,29 +1,40 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 import { Button, Link, Stack, Typography } from "@mui/material";
 import { Download } from "@mui/icons-material";
+
+import { BASE_URL } from "../../types/http";
 
 interface IDownloadMediaProps {
   media: string[] | null;
   fetchMedia: () => void;
+  title: string;
 }
 
-
-const DownloadMedia: FC<IDownloadMediaProps> = ({media, fetchMedia}) => {
+const DownloadMedia: FC<IDownloadMediaProps> = ({
+  media,
+  fetchMedia,
+  title,
+}) => {
   return (
     <Stack>
-      <Typography fontSize={20}>Неподписанные документы</Typography>
-      {!media && <Button variant="contained" onClick={fetchMedia}>Получить</Button>}
-      {media?.map((media, idx) =>
+      <Typography fontSize={20}>{title}</Typography>
+      {!media && (
+        <Button variant="contained" onClick={fetchMedia}>
+          Получить
+        </Button>
+      )}
+      {media?.map((media, idx) => (
         <Link
           fontSize={20}
-          sx={{display: 'flex', alignItems: 'center'}}
-          href={media}
+          sx={{ display: "flex", alignItems: "center" }}
+          href={`${BASE_URL}/${media}`}
           key={idx}
-          target='_blank'
+          target="_blank"
           download
         >
-          <Download/> Скачать
-        </Link>)}
+          <Download /> Скачать
+        </Link>
+      ))}
     </Stack>
   );
 };
